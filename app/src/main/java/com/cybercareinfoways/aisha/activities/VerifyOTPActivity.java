840e2850b221e5e9df2013d7c010e74f7e84b49b
amritrath327 <amritrath327@gmail.com>
2017-03-01 23:53:31 +0530

    get dial code

---
+++ app/src/main/java/com/cybercareinfoways/aisha/activities/VerifyOTPActivity.java
@@ -0,0 +1,9 @@
+package com.cybercareinfoways.aisha.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by YELOWFLASH on 02/28/2017.
+ */
+public class VerifyOTPActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/cybercareinfoways/helpers/AppConstants.java
+++ app/src/main/java/com/cybercareinfoways/helpers/AppConstants.java
@@ -6,4 +6,13 @@ package com.cybercareinfoways.helpers;
 
 public class AppConstants {
     public static final String REGISTERTITLE = "Register";
+    public static final String TERMS = "Terms & Conditions";
+    public static final String CONFIRMDIALOG = "Are you sure you want to continue with ";
+    public static final String PHOENERROR = "Enter Valid Phone Number";
+    public static final String ACCECPTTERMS = "Accept the Terms & Conditions to continue";
+
+    public static final String MOBILENUMBER = "mobile_number";
+
+    public static final String COUNTRYCODE = "dial_code";
+    public static final String COUNTRYNAME = "name";
 }
